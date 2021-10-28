7f0528cd2e874b57bed8d01c2a430726353118f4
ngohoanglong <ngohoanglongptit@gmail.com>
2021-10-28 11:52:03 +0700

    init

---
+++ src/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  mode: 'jit',
+  future: {
+    purgeLayersByDefault: true,
+    applyComplexClasses: true,
+  },
+  purge: [
+    './**/*.liquid',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    namedGroups: ['one', 'two'],
+    extend: {
+      
+    },
+    
+  },
+  plugins: [
+    
+  ],
+  
+}
